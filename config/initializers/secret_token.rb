76926f63092de1667b4ff8de127db5e4e3743ac4
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-06-16 20:45:08 +0530

    Changes till first test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '68609521069f2c229de4165e24408958eb2fff5ae1d6be926eb6792a96008111af8587445fa13c619724a1ff9ec4d92402885d7e8c672e2e9bca3b12dfe0d215'
