6e93be89ec82573c732aa001619a2b8b9c606cd7
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-06-26 15:37:16 +0530

    completed till page_390

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,47 @@
 class UsersController < ApplicationController
-  
-  def show
-  	@user=User.find(params[:id])
-  end
+ before_filter :signed_in_user, only: [:edit, :update]
 
-  def index
-  end
-  
-  def new
-    @user=User.new
-  end
+ def show
+   @user=User.find(params[:id])
+ end
 
-  def create
-    @user=User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success]="Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-  
-  def edit
-  end
+ def index
+ end
+ 
+ def new
+  @user=User.new
+end
 
-  def update
+def create
+  @user=User.new(params[:user])
+  if @user.save
+    sign_in @user
+    flash[:success]="Welcome to the Sample App!"
+    redirect_to @user
+  else
+    render 'new'
   end
+end
 
-  def destroy
-  end
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(params[:user])
+   flash[:success] = "Profile updated"
+   sign_in @user
+   redirect_to @user
+ else
+  render 'edit'
+end
+end
+def destroy
+end
+
+private
+def signed_in_user
+  redirect_to signin_path, notice: "Please sign in." unless signed_in?    
+end
 end
