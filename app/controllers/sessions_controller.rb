7a1a28fb4a8333d5f17815077232224ed6c1f851
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-06-26 17:13:36 +0530

    completed til code 396

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user=User.find_by_email(params[:session][:email])
+		user = User.find_by_email(params[:session][:email])
 		if user && user.authenticate( params[:session][:password] )
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
-			flash.now[:error]='Invalid email / password combination'
+			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
 		end
 	end
@@ -17,5 +17,4 @@ class SessionsController < ApplicationController
 		sign_out
 		redirect_to root_path
 	end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
  before_filter :signed_in_user, only: [:edit, :update]
+ before_filter :correct_user, only: [:edit, :update]
+
 
  def show
    @user=User.find(params[:id])
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
 
  def index
  end
- 
+
  def new
   @user=User.new
 end
@@ -28,10 +30,9 @@ def edit
 end
 
 def update
-  @user = User.find(params[:id])
   if @user.update_attributes(params[:user])
    flash[:success] = "Profile updated"
-   sign_in @user
+   sign_in @user   
    redirect_to @user
  else
   render 'edit'
@@ -42,6 +43,14 @@ end
 
 private
 def signed_in_user
-  redirect_to signin_path, notice: "Please sign in." unless signed_in?    
+  unless signed_in?
+    store_location
+    redirect_to signin_path, notice: "Please sign in."
+  end  
+end
+
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_path) unless current_user?(@user)
 end
 end
