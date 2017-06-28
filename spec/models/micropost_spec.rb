c854bc413c9899201dc542a5b1acef8027aa7342
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-06-28 12:27:18 +0530

    completed till page 438

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -7,10 +7,11 @@ describe Micropost do
 
 	subject { @micropost }
 
-	it {should respond_to(:content)}
-	it {should respond_to(:user_id)}
-	it {should respond_to(:user)}
+	it { should respond_to(:content) }
+	it { should respond_to(:user_id) }
+	it { should respond_to(:user) }
 	its (:user) { should == user }
+	
 	it { should be_valid }
 
 	describe "accessible attributes" do
